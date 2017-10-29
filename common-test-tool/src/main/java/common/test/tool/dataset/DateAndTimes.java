81bd6f340f2abc4f64e7c8c208b6db502cab9a22
Riadh MNASRI <riadh.mnasri@gmail.com>
2017-10-29 14:12:07 +0100

    Change Meta Data

--- common-test-tool/src/main/java/common/test/tool/dataset/DateAndTimes.java
+++ common-test-tool/src/main/java/common/test/tool/dataset/DateAndTimes.java
@@ -16,8 +16,8 @@ public class DateAndTimes {
     public static final LocalTime LT_12100000 = LocalTime.of(12, 10);
     public static final LocalDateTime LDT_20150618_23073050 = LocalDateTime.of(2015, 6, 18, 23, 7, 30, 500000000);
     public static final ZonedDateTime
-        ZDT_20150618_23073050 = ZonedDateTime.of(LDT_20150618_23073050, ZoneId.of("Asia/Tokyo"));
+        ZDT_20150618_23073050 = ZonedDateTime.of(LDT_20150618_23073050, ZoneId.of("Europe/Paris"));
 
-    public static final Date OLD_DATE_20150618 = Date.valueOf("2015-06-18");
-    public static final Timestamp OLD_TIMESTAMP_20150618_23073050 = Timestamp.valueOf("2015-06-18 23:07:30.5");
+    public static final Date OLD_DATE_20150618 = Date.valueOf("2017-10-28");
+    public static final Timestamp OLD_TIMESTAMP_20150618_23073050 = Timestamp.valueOf("2017-10-28 22:30:15.3");
 }
